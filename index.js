b16896c7c7a21f638c43fd7d62f9cec54417efdb
Michael Leer <michael.leer@rackspace.co.uk>
2015-06-24 19:48:01 +0100

    Multiple changes / additions [*] /git - link to github [*] /help - useragent checking [*] process arguments [*] JSLint

--- index.js
+++ index.js
@@ -1,51 +1,76 @@
-var express =require('express'),
+var express = require('express'),
     genPass = require('password-generator');
-
-
 
 var app = express();
 
-app.get('/custom/:len', function(req, res) {
-	if(isNaN(req.params.len)){
-		res.status(400).send('must be a number\n').end();
-	}
-	else{
-		res.status(200).send(genPass(req.params.len, false) + '\n');
-	}
+app.locals.title = 'canihaz.pw';
+app.locals.email = 'me@trozzy.net';
+app.disable('etag');
+app.disable('x-powered-by');
+
+app.get('/custom/:len', function (req, res) {
+    "use strict";
+    if (isNaN(req.params.len)) {
+        res.status(400).send('must be a number\r\n').end();
+    } else {
+        res.status(200).send(genPass(req.params.len, false) + '\r\n');
+    }
 });
-app.get('/custom/:len/:mem', function(req, res) {
-	if(req.params.mem != 'true' && req.params.mem != 'false'){
-		res.status(400).send('must be true / false\n').end();
-	}
-	else{
-		res.status(200).send(genPass(req.params.len, req.params.mem) + '\n');
-	}
+app.get('/custom/:len/:mem', function (req, res) {
+    "use strict";
+    if (req.params.mem !== 'true' && req.params.mem !== 'false') {
+        res.status(400).send('must be true / false\r\n').end();
+    } else {
+        res.status(200).send(genPass(req.params.len, req.params.mem) + '\r\n');
+    }
 });
 
-app.get('/prefix/:pre', function(req, res) {
-	var length = req.params.pre.length + 12;
-	res.status(200).send(genPass(length, false,'',req.params.pre) + '\n');
+app.get('/prefix/:pre', function (req, res) {
+    "use strict";
+    var length = req.params.pre.length + 12;
+    res.status(200).send(genPass(length, false, '', req.params.pre) + '\r\n');
 });
 
-app.get('/prefix/:pre/:len', function(req, res) {
-        if(isNaN(req.params.len)){
-		res.status(400).send('must be a number\n').end();
-	}
-        else{
-		res.status(200).send(genPass(req.params.len, false,'',preq.params.pre) + '\n');
-	}
+app.get('/prefix/:pre/:len', function (req, res) {
+    "use strict";
+    if (isNaN(req.params.len)) {
+        res.status(400).send('must be a number\r\n').end();
+    } else {
+        var length = req.params.pre.length + req.params.len;
+        res.status(200).send(genPass(length, false, '', req.params.pre) + '\r\n');
+    }
 });
 
-app.get('/help', function(req, res) {
-  res.status(200).send('canihaz.pw is a simple password generator\n\n Options\n----------\n/custom/$length\n/custom/$length/$memorable\n/prefix/$prefix\n/prefix/$prefix/$length\n');
+// Present a help page for usage in command line or browser
+app.get('/help', function (req, res) {
+    "use strict";
+    var newline;
+    if (req.headers['user-agent'].indexOf("Mozilla") > -1 || req.headers['user-agent'].indexOf("Chrome") > -1 || req.headers['user-agent'].indexOf("MSIE") > -1 || req.headers['user-agent'].indexOf("Opera") > -1) {
+        newline = "<br/>";
+    } else {
+        newline = "\r\n";
+    }
+    res.status(200).send('canihaz.pw is a simple password generator' + newline + newline + 'Options' + newline + '----------' + newline + '/custom/$length' + newline + '/custom/$length/$memorable' + newline + '/prefix/$prefix' + newline + '/prefix/$prefix/$length' + newline);
 });
 
-app.get('/about', function(req, res) {
-  res.status(200).send('canihaz.pw is provided by Michael Leer (Trozz) and is hosted on the Rackspace Public Cloud\n');
+// Present a simple about page
+app.get('/about', function (res) {
+    "use strict";
+    res.status(200).send('canihaz.pw is provided by Michael Leer (Trozz) and is hosted on the Rackspace Public Cloud\r\n');
 });
 
-app.use(function(req, res) {
-  res.status(200).send(genPass(12, false) + '\n');
+// Github Link
+app.get('/git', function (res) {
+    "use strict";
+    res.status(200).send('canihaz.pw source is available on Github @ https://github.com/Trozz/canihaz.pw\r\n');
 });
 
-app.listen(5432);
+//app.use(device.capture());
+app.use(function (res) {
+    "use strict";
+    res.status(200).send(genPass(12, false) + '\r\n');
+});
+
+
+// Pass port as arguement
+app.listen(process.argv[2]);
